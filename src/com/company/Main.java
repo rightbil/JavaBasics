f61e364c027a72f116e57a26a6bd4c177f850859
rightbil@gmail.com <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-22 16:22:13 -0500

    Update Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //Write an application that assigns 2 to a variable named mynumber
+        // and 1.7938 to a variable namedmyothernumber, and then prints out the values to the console.
+        int myNumber = 2;
+        double namedMyOhterNubmer = 1.7938;
+        System.out.println("My Number = " + myNumber +"\nMy Other Number = " + namedMyOhterNubmer);
+
     }
 }
